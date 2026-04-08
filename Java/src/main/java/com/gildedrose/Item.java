cd123044b4da621269c4a76bd29101cdf082e9d2
Viktor Kalashnykov <viktor.kalashnykov@infinitedata.com>
2026-04-08 22:33:50 +0300

    Improvement: Upgraded Gradle to version 8.9. Improvement: added equals() and hasCode() methods to com.gildedrose.Item class in Java project

--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -1,5 +1,7 @@
 package com.gildedrose;
 
+import java.util.Objects;
+
 public class Item {
 
     public String name;
@@ -17,5 +19,20 @@ public class Item {
    @Override
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
+    }
+
+    // equals() and hashCode() must be overwrritten for correctly comparing 2 instances of Item class
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return Objects.equals(name, item.name) && sellIn == item.sellIn && quality == item.quality;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, sellIn, quality);
     }
 }
